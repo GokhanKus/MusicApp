1cfbd014878d0e026789015fcf82a42b669ded38
GokhanKus <gkus1998@hotmail.com>
2023-10-11 14:25:13 +0300

    SongController Şarkı Sayfası, tür listesi

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApp.Data;
+using MusicApp.Models;
+
+namespace MusicApp.Controllers
+{
+	public class SongsController : Controller
+	{
+		private readonly SongContext _context;
+        public SongsController(SongContext context)
+        {
+			_context = context;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult List(int? id, string q)
+		{
+			var songs = _context.Songs;
+
+			var model = new SongsViewModel
+			{
+				Songs = songs.ToList()
+			};
+
+			return View("Songs",model);			//bu sekilde yaparsak List.cshtml adında dosya olusturmak yerine Songs.cshtml dosyası olusturabiliriz.
+												//view tarafından renderlanacak olan modeli olusturalım
+		}
+	}
+}
---
+++ ViewComponents/GenresViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApp.Data;
+
+namespace MusicApp.ViewComponents
+{
+	public class GenresViewComponent:ViewComponent
+	{
+		private readonly SongContext _context; //veritabanı ile etkilesim icin kullanılır
+		public GenresViewComponent(SongContext context)
+		{
+			_context = context;  //injection islemi  
+		}
+		public IViewComponentResult Invoke()
+		{
+
+			ViewBag.SelectedGenre = RouteData.Values["id"];   //secilen film türünün isaretlenmesi(background mavi olsun mesela)
+
+			return View(_context.Genres.ToList());
+		}
+	}
+}
