aaac7a037e3d3d5f041e89428c2b5f8d2fc1b8b6
GokhanKus <gkus1998@hotmail.com>
2023-10-12 12:15:25 +0300

    Şarkı Ekleme Sayfası (Admin Panel)

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicApp.Data;
+using MusicApp.Entity;
+using MusicApp.Models;
+
+namespace MusicApp.Controllers
+{
+	public class AdminController : Controller
+	{
+		private readonly SongContext _context;
+		public AdminController(SongContext context)
+		{
+			_context = context; //inject işlemi
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult AddSong()
+		{
+			ViewBag.Genres = _context.Genres.ToList();
+			return View(new AdminCreateSongModel());
+		}
+
+		[HttpPost]
+		public IActionResult AddSong(AdminCreateSongModel model) //model bilgilerini sayfaya post edeceğiz
+		{
+			//if (model.Name == null)
+			//{
+			//	ModelState.AddModelError("", "Please enter song's name");
+			//}
+
+			//if (ModelState.IsValid)
+			//{
+
+			//}
+			var entity = new Song
+			{
+				Name = model.Name,
+				Description = model.Description,
+				ImageUrl = "No_Image.jpg"
+			};
+			foreach (var id in model.GenreIds)
+			{
+				entity.Genres.Add(_context.Genres.FirstOrDefault(i => i.GenreId == id));
+			}
+			_context.Songs.Add(entity);
+			_context.SaveChanges();
+
+			//return RedirectToAction("SongList");
+			return View();
+		}
+		public IActionResult SongList()
+		{
+			var model = new AdminSongsViewModel
+			{
+				Songs = _context.Songs  
+				.Include(s => s.Genres)						//Include() metodunu yazarak songs'un genre bilgilerini aldık left join yaptık
+				.Select(s => new AdminSongViewModel
+				{
+					SongId=s.SongId,
+					Name = s.Name,
+					Description= s.Description,
+					ImageUrl= s.ImageUrl,
+					Genres = s.Genres.ToList()
+				}).ToList()
+			};
+			return View(model);
+		}
+	}
+}
---
+++ Models/AdminSongsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MusicApp.Entity;
+
+namespace MusicApp.Models
+{
+	public class AdminSongsViewModel
+	{
+		public List<AdminSongViewModel> Songs { get; set; }
+	}
+	public class AdminSongViewModel
+	{
+		public int SongId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string ImageUrl { get; set; }
+		public List<Genre> Genres { get; set; } //many to many relation with Genre
+	}
+	public class AdminCreateSongModel
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int[] GenreIds { get; set; }
+		public DateTime ReleaseDate { get; set; } = DateTime.Now;
+	}
+}
