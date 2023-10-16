a96d81b3add5a0acc632ecd6e13bd9b74e16ad8d
GokhanKus <gkus1998@hotmail.com>
2023-10-16 11:32:18 +0300

    Şarkı türüne göre ve aranan kelimeye göre filtreleme

--- Controllers/SongsController.cs
+++ Controllers/SongsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MusicApp.Data;
 using MusicApp.Models;
 
@@ -15,17 +16,30 @@ namespace MusicApp.Controllers
 		{
 			return View();
 		}
-		public IActionResult List(int? id, string q)
+		public IActionResult List(int? id, string q)					//id türleri q ise aranan kelimeyi temsil eder. id kısmında türe göre q kısmında aranan kelimeye göre filtreler
 		{
-			var songs = _context.Songs;
+			var searchingWord = q;										//buraya şarkı, şarkıcı ararken yazılan kelime gelir (model binding)
 
+			var songs = _context.Songs.AsQueryable();
+			
+			if (id != null)												//burası şarkı türüne göre filtreleme yapacak
+			{
+				songs = songs
+					.Include(s => s.Genres)
+					.Where(s => s.Genres.Any(g => g.GenreId == id));
+			}
+			if (!string.IsNullOrEmpty(q))								//burası aranan kelimeye göre filtreleme yapacak
+			{
+				songs = songs
+					.Where(s => s.Name.Contains(q) 
+					|| s.Description.Contains(q));
+			}
 			var model = new SongsViewModel
 			{
 				Songs = songs.ToList()
 			};
 
-			return View("Songs",model);			//bu sekilde yaparsak List.cshtml adında dosya olusturmak yerine Songs.cshtml dosyası olusturabiliriz.
-												//view tarafından renderlanacak olan modeli olusturalım
+			return View("Songs",model);									//bu sekilde yaparsak List.cshtml adında dosya olusturmak yerine Songs.cshtml dosyası olusturabiliriz.
 		}
 	}
 }
