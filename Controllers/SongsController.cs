6718a9e772ee3c8fcc086ef907ff1e162f7c131b
GokhanKus <gkus1998@hotmail.com>
2023-10-20 09:50:00 +0300

    string.join kullanım ve viewbag (birden fazla genre veya sanatçı bilgisi varsa yan yana yazdıralım)

--- Controllers/SongsController.cs
+++ Controllers/SongsController.cs
@@ -8,30 +8,30 @@ namespace MusicApp.Controllers
 	public class SongsController : Controller
 	{
 		private readonly SongContext _context;
-        public SongsController(SongContext context)
-        {
+		public SongsController(SongContext context)
+		{
 			_context = context;
-        }
-        public IActionResult Index()
+		}
+		public IActionResult Index()
 		{
 			return View();
 		}
-		public IActionResult List(int? id, string q)					//id türleri q ise aranan kelimeyi temsil eder. id kısmında türe göre q kısmında aranan kelimeye göre filtreler
+		public IActionResult List(int? id, string q)                    //id türleri q ise aranan kelimeyi temsil eder. id kısmında türe göre q kısmında aranan kelimeye göre filtreler
 		{
-			var searchingWord = q;										//buraya şarkı, şarkıcı ararken yazılan kelime gelir (model binding)
+			var searchingWord = q;                                      //buraya şarkı, şarkıcı ararken yazılan kelime gelir (model binding)
 
 			var songs = _context.Songs.AsQueryable();
-			
-			if (id != null)												//burası şarkı türüne göre filtreleme yapacak
+
+			if (id != null)                                             //burası şarkı türüne göre filtreleme yapacak
 			{
 				songs = songs
 					.Include(s => s.Genres)
 					.Where(s => s.Genres.Any(g => g.GenreId == id));
 			}
-			if (!string.IsNullOrEmpty(q))								//burası aranan kelimeye göre filtreleme yapacak
+			if (!string.IsNullOrEmpty(q))                               //burası aranan kelimeye göre filtreleme yapacak
 			{
 				songs = songs
-					.Where(s => s.SongName.Contains(q) 
+					.Where(s => s.SongName.Contains(q)
 					|| s.Description.Contains(q));
 			}
 			var model = new SongsViewModel
@@ -39,7 +39,7 @@ namespace MusicApp.Controllers
 				Songs = songs.ToList()
 			};
 
-			return View("Songs",model);									//bu sekilde yaparsak List.cshtml adında dosya olusturmak yerine Songs.cshtml dosyası olusturabiliriz.
+			return View("Songs", model);                                    //bu sekilde yaparsak List.cshtml adında dosya olusturmak yerine Songs.cshtml dosyası olusturabiliriz.
 		}
 		public IActionResult Details(int id)
 		{
@@ -50,13 +50,16 @@ namespace MusicApp.Controllers
 			şarkının artistnamesi var ise detay sayfasında gözükecek.*/
 			//aynı şekilde Genres bilgilerini de bu yüzden eklemek zorundayız.
 
-			var model = _context.Songs.Include(s => s.Artists).Include(g=>g.Genres).FirstOrDefault(s => s.SongId == id);
+			var model = _context.Songs.Include(s => s.Artists).Include(g => g.Genres).FirstOrDefault(s => s.SongId == id);
 
 			//eğer 1 şarkıyı birden fazla sanatçı söylemişse detay kısmında sanatçılar alt alta çıkıyordu, bunu istemedim yan yana olsun istedim. string.Join()
 			var artists = model.Artists.Select(a => a.ArtistName).ToList();
 			var artistNames = string.Join(", ", artists);
 			ViewBag.ArtistNames = artistNames;
 
+			var genres = model.Genres.Select(g => g.GenreName).ToList();
+			var genreNames = string.Join("&", genres);
+			ViewBag.GenreNames = genreNames;
 
 			return View(model);
 		}
