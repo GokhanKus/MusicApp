0b25bccb7b3c33653088d3a108b4a32137593ea6
GokhanKus <gkus1998@hotmail.com>
2023-10-30 17:18:10 +0300

    Login ve Register Sayfaları, ViewModelleri

---
+++ IdentityModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicApp.IdentityModels
+{
+	public class RegisterViewModel
+	{
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage = "Passwords do not macth")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
