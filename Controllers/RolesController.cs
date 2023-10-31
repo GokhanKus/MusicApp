7523238975689141f04079f180eabece8dfaeefb
GokhanKus <gkus1998@hotmail.com>
2023-10-31 12:52:52 +0300

    Role Create etme ve role sayfası

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MusicApp.Identity;
 
 namespace MusicApp.Controllers
 {
 	public class RolesController : Controller
 	{
-		public IActionResult RoleList()
+		private readonly RoleManager<AppRole> _roleManager;
+        public RolesController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult RoleList()
+		{
+			var model = _roleManager.Roles;
+			return View(model);
+		}
+
+		public IActionResult RoleCreate()
 		{
 			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> RoleCreate(AppRole model)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await _roleManager.CreateAsync(model);
+
+				if (result.Succeeded)
+				{
+					return RedirectToAction("RoleList");
+				}
+				foreach (var err in result.Errors) //eger hata/hatalar varsa yazdıralım
+				{
+					ModelState.AddModelError("", err.Description);
+				}
+			}
+			return View(model);
 		}
 	}
 }
