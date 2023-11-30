c3e1fa8f329176d8f2acf32d274e5619085f7d7d
GokhanKus <gkus1998@hotmail.com>
2023-11-30 17:34:23 +0300

    IPagedList

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,28 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MusicApp.Data;
+using MusicApp.Entity;
 using MusicApp.Models;
 using System.Diagnostics;
+using X.PagedList;
 
 namespace MusicApp.Controllers
 {
-	
+
 	public class HomeController : BaseController
 	{
 		//private readonly SongContext _context;
 
 		//basecontrollerdan kalıtım aldığımız için contexti injection islemini burada yapmayalım
 		public HomeController(SongContext context) : base(context)
-        {
-			//_context = songContext; //injection islemi
-        }
-        public IActionResult Index()
 		{
+			//_context = songContext; //injection islemi
+		}
+		public IActionResult Index(int page = 1)
+		{
+			//	var songs = _context.Songs.ToList();
+			//	IPagedList<Song> pagedSongs = songs.ToPagedList(page, 10);
 			var model = new HomePageViewModel
 			{
 				PopularSongs = _context.Songs.ToList()
 			};
-			return View(model);
+			IPagedList<Song> pagedList = model.PopularSongs.ToPagedList(page, 4);// 1.sayfadan baslasin ve 4 item gostersin
+
+			return View(pagedList);
 		}
 
 		public IActionResult Privacy()
